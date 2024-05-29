5e008a4be3e15bfc9439586c306bbd280e94dfe2
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-29 17:47:58 +0530

    Working on a FoodCard Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import CategoryMenu from "../components/CategoryMenu";
+import FoodItems from "../components/FoodItems";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <CategoryMenu/>
+      <FoodItems/>
     </>
   );
 };
