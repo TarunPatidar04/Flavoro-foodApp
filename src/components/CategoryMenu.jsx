9c689f0cd3a822826226a0fd28e6aa6341e832a6
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-01 20:38:22 +0530

    create a filter method on a categoryMenu

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,30 +1,51 @@
 import React, { useEffect, useState } from "react";
 import FoodData from "../data/FoodData";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory } from "../redux/slices/CategorySlice";
 
 const CategoryMenu = () => {
-  const [Category, setCategory] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const listUniqueCategory = () => {
-    const uniqueCategory = [...new Set(FoodData.map((food) => food.category))];
-    console.log(uniqueCategory);
-    setCategory(uniqueCategory);
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+    console.log(uniqueCategories);
   };
+
   useEffect(() => {
-    listUniqueCategory();
+    listUniqueCategories();
   }, []);
+
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector((state) => state.category.category);
+
   return (
     <div className="ml-6">
-      <h3 className="text-xl font-semibold">Find the best Food</h3>
-      <div className="my-5 flex gap-4 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
-        {Category.map((category) => (
-          <button
-            className="px-3 py-2 bg-gray-200 font-bold
-        rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-            key={category}
-          >
-            {category}
-          </button>
-        ))}
+      <h3 className="text-xl font-semibold">Find the best food</h3>
+      <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
+        <button
+          onClick={() => dispatch(setCategory("All"))}
+          className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${
+            selectedCategory === "All" && "bg-green-500 text-white"
+          }`}
+        >
+          All
+        </button>
+        {categories.map((category, index) => {
+          return (
+            <button
+              onClick={() => dispatch(setCategory(category))}
+              key={index}
+              className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${
+                selectedCategory === category && "bg-green-500 text-white"
+              } `}
+            >
+              {category}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
