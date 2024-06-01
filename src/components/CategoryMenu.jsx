8387e73763db994fb74f240c2d17417a63d19432
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-01 19:58:58 +0530

    Extracting a unique category

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,40 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FoodData from "../data/FoodData";
 
 const CategoryMenu = () => {
+  const [Category, setCategory] = useState([]);
+
+  const listUniqueCategory = () => {
+    const uniqueCategory = [...new Set(FoodData.map((food) => food.category))];
+    console.log(uniqueCategory);
+    setCategory(uniqueCategory);
+  };
+  useEffect(() => {
+    listUniqueCategory();
+  }, []);
   return (
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best Food</h3>
       <div className="my-5 flex gap-4 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
-        <button
-          className="px-3 py-2 bg-gray-200 font-bold
+        {Category.map((category) => (
+          <button
+            className="px-3 py-2 bg-gray-200 font-bold
         rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-        >
-          All
-        </button>
-        <button
-          className="px-3 py-2 bg-gray-200 font-bold
-        rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-        >
-          Lunch
-        </button>
-        <button
-          className="px-3 py-2 bg-gray-200 font-bold
-        rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-        >
-          Breakfast
-        </button>
-        <button
-          className="px-3 py-2 bg-gray-200 font-bold
-        rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-        >
-          Dinner
-        </button>
-        <button
-          className="px-3 py-2 bg-gray-200 font-bold
-        rounded-lg hover:bg-green-600 hover:text-white cursor-pointer"
-        >
-          Snacks
-        </button>
+            key={category}
+          >
+            {category}
+          </button>
+        ))}
       </div>
     </div>
   );
